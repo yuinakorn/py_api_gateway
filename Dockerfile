62acf8a20a8ae40f88cdc390d5b16d5158df554b
yuinakorn <yuinakorn@hotmail.com>
2024-05-21 11:39:55 +0700

    error status

--- Dockerfile
+++ Dockerfile
@@ -17,4 +17,4 @@ COPY . .
 EXPOSE 8000
 
 # Set the command to start Gunicorn
-CMD ["gunicorn", "main:app", "-w", "4", "-k", "uvicorn.workers.UvicornWorker", "-b", "0.0.0.0:8000"]
+CMD ["gunicorn", "main:app", "-w", "8", "-k", "uvicorn.workers.UvicornWorker", "-b", "0.0.0.0:8000"]
--- controllers/query_controller.py
+++ controllers/query_controller.py
@@ -2,6 +2,9 @@ import json
 
 import httpx
 
+import urllib.request
+import urllib.error
+
 from dotenv import dotenv_values
 
 config_env = dotenv_values(".env")
@@ -23,9 +26,22 @@ def get_status_all():
 
 
 def get_status_by_hoscode(hoscode):
-    # use httpx to get data from api
-    url = f"{config_env['API_URL']}/query/status/{hoscode}"
-    response = httpx.get(url, headers={})
-    result = response.json()
+    try:
+        # Use httpx to get data from API
+        url = f"{config_env['API_URL']}/query/status/{hoscode}"
+        request = httpx.Request('GET', url)
+        response = httpx.get(url, headers={}, timeout=6.0)  # Set a timeout value
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        result = response.json()
+        return result
+
+    except httpx.ReadTimeout:
+        return {"status_code": 408, "error": "Request Timeout"}
+    except httpx.HTTPStatusError as http_err:
+        result = {"status_code": 400, "error": f"HTTP error occurred: {http_err}"}
+    except httpx.RequestError as req_err:
+        result = {"status_code": 400, "error": f"Request error occurred: {req_err}"}
+    except Exception as err:
+        result = {"status_code": 500, "error": f"An unexpected error occurred: {err}"}
 
     return result
